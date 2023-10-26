6aa96f289c594e3a4ac17fb1ccaadf60a752e2f0
stanislava-pavlovaa <stanislava.pavlova@c4nexus.com>
2023-10-26 19:57:52 +0300

    Fixed bugs

--- cartridges/app_storefront_custom/cartridge/models/account.js
+++ cartridges/app_storefront_custom/cartridge/models/account.js
@@ -4,14 +4,14 @@ var base = module.superModule;
 
 /**
  * Account class that represents the current customer's profile dashboard
- * @param {Object} currentCustomer - Current customer
+ * @param {Object} profile - Current customer profile
  * @param {Object} addressModel - The current customer's preferred address
  * @param {Object} orderModel - The current customer's order history
  * @constructor
  */
 
-function account(profile, addressBook, orderModel) {
-    base.call(this, profile, addressBook, orderModel);
+function account(profile, addressModel, orderModel) {
+    base.call(this, profile, addressModel, orderModel);
 
     this.profile.residence = customer.profile.getCustom().residence;
     this.profile.interests = customer.profile.getCustom().interests;
