24e0160f537f820e84f51d1b84033bd3bf710918
stanislava-pavlovaa <stanislava.pavlova@c4nexus.com>
2023-11-01 09:18:45 +0200

    Jobs demo

---
+++ cartridges/app_storefront_custom/cartridge/scripts/steps/DemoTestModuleJob.js
@@ -0,0 +1,33 @@
+var CustomObjectMgr = require("dw/object/CustomObjectMgr");
+var { File, FileWriter, XMLStreamWriter } = require("dw/io");
+
+module.exports.execute = function () {
+    var demoObjectIterator = CustomObjectMgr.getAllCustomObjects("DemoObject");
+
+    var file;
+    var fileWriter;
+    var xsw;
+
+    try {
+        file = new File([File.IMPEX, "test", "text.xml"].join(File.SEPARATOR));
+        fileWriter = new FileWriter(file);
+
+        xsw = new XMLStreamWriter(fileWriter);
+
+        xsw.writeStartDocument();
+        xsw.writeStartElement("products");
+
+        while (demoObjectIterator.hasNext()) {
+            var demo = demoObjectIterator.next();
+            xsw.writeStartElement("product");
+            xsw.writeAttribute("id", demo.custom.product);
+            xsw.writeAttribute("name", demo.custom.name);
+            xsw.writeEndElement();
+        }
+        xsw.writeEndElement();
+    } catch (error) {
+    } finally {
+        xsw.close();
+        fileWriter.close();
+    }
+};
