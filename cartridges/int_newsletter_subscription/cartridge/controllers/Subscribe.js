96ff5a0f0ae66cae04de70f2a5dbd11dbee38062
stanislava-pavlovaa <stanislava.pavlova@c4nexus.com>
2023-11-02 17:53:22 +0200

    Created form for Newsletter Subscription

--- cartridges/int_newsletter_subscription/cartridge/controllers/Subscribe.js
+++ cartridges/int_newsletter_subscription/cartridge/controllers/Subscribe.js
@@ -1,86 +1,94 @@
 "use strict";
 
+/**
+ * @namespace Subscribe
+ */
+
 var server = require("server");
-var Transaction = require("dw/system/Transaction");
-var CustomObjectMgr = require("dw/object/CustomObjectMgr");
-var UUIDUtils = require("dw/util/UUIDUtils");
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
 
 /**
- * Create Custom Object
+ * Subscribe-Show : The Subscribe-Show endpoint renders the page that allows a shopper to subscribe for newsletter.
+ * @name Base/Subscribe-EditProfile
+ * @function
+ * @memberof Subscribe
+ * @param {middleware} - server.middleware.https
+ * @param {middleware} - csrfProtection.generateToken
+ * @param {middleware} - consentTracking.consent
+ * @param {renders} - isml
+ * @param {serverfunction} - get
  */
+server.get("Show", server.middleware.https, csrfProtection.generateToken, consentTracking.consent,
+    function (req, res, next) {
+        var newsletterForm = server.forms.getForm("newsletter");
+        newsletterForm.clear();
 
-server.post("Create", server.middleware.https, function (req, res, next) {
-    var form = req.form;
-    var error = false;
-
-    if (!form) {
-        error = true;
+        res.render("subscribe/newsletter", {
+            newsletterForm: newsletterForm,
+        });
+        next();
     }
+);
 
-    var type = "Newsletter_Subscription";
-    var keyValue = UUIDUtils.createUUID();
+/**
+ * Subscribe-Create : The Subscribe-Create endpoint is the endpoint that gets hit when a shopper has subscribed for newsletter
+ * @name Base/Subscribe-Create
+ * @param {middleware} - server.middleware.https
+ * @param {middleware} - csrfProtection.validateAjaxRequest
+ * @param {httpparameter} - csrf_token - hidden input field CSRF token
+ * @param {returns} - json
+ * @param {serverfunction} - post
+ */
+server.post("Create", server.middleware.https, csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+        var CustomObjectMgr = require("dw/object/CustomObjectMgr");
+        var Transaction = require("dw/system/Transaction");
+        var Resource = require("dw/web/Resource");
+        var URLUtils = require("dw/web/URLUtils");
 
-    try {
-        Transaction.wrap(function () {
-            if (!form.email || !form.firstName || !form.lastName) {
-                error = true;
-            } else {
-                var newsletter = CustomObjectMgr.createCustomObject(type, keyValue);
-                newsletter.custom.email = form.email;
-                newsletter.custom.firstName = form.firstName;
-                newsletter.custom.lastName = form.lastName;
-                if (form.gender) {
-                    newsletter.custom.gender = form.gender;
+        var newsletterForm = server.forms.getForm("newsletter");
+        var NEWSLETTER = "Newsletter_Subscription";
+
+        var newsletterResult = CustomObjectMgr.getCustomObject(
+            NEWSLETTER,
+            newsletterForm.email.value
+        );
+
+        if (!empty(newsletterResult)) {
+            newsletterForm.valid = false;
+            newsletterForm.email.valid = false;
+            newsletterForm.email.error = Resource.msg(
+                "error.message.not.unique",
+                "forms",
+                null
+            );
+            res.json({
+                success: false,
+                errorMessage: Resource.msg('error.message.not.unique', 'forms', null)
+            });
+        }
+
+        if (newsletterForm.valid) {
+            Transaction.wrap(function () {
+                var newsletterEntry = CustomObjectMgr.createCustomObject(
+                    NEWSLETTER,
+                    newsletterForm.email.value
+                );
+                newsletterEntry.custom.firstName = newsletterForm.firstName.value;
+                newsletterEntry.custom.lastName = newsletterForm.lastName.value;
+                if (newsletterForm.gender.value) {
+                    newsletterEntry.custom.gender = newsletterForm.gender.value;
                 }
-            }
-        });
-    } catch (error) {
-        error = true;
-    }
-
-    if (error) {
-        res.json({ error: true });
-    } else {
+            });
+        }
         res.json({
-            id: keyValue,
-            error: false,
+            success: true,
+            redirectUrl: URLUtils.url("Subscribe-Show").toString(),
         });
+
+        return next();
     }
-    return next();
-});
-
-/**
- * Delete Custom Object
- */
-server.post("Delete", server.middleware.https, function (req, res, next) {
-    var form = req.form;
-    var error = false;
-
-    if (!form) {
-        error = true;
-    }
-
-    var type = "Newsletter_Subscription";
-    var keyValue = form.id;
-
-    try {
-        var newsletter = CustomObjectMgr.createCustomObject(type, keyValue);
-        Transaction.wrap(function () {
-            CustomObjectMgr.remove(newsletter);
-        });
-    } catch (error) {
-        error = true;
-    }
-
-    if (error) {
-        res.json({ error: true });
-    } else {
-        res.json({
-            id: keyValue,
-            error: false,
-        });
-    }
-    return next();
-});
+);
 
 module.exports = server.exports();
